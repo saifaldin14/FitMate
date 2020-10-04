65289cb010c00c07ffb7f3576e91ef215c60ac53
saifaldin14 <saifaldindeveloper@gmail.com>
2020-10-03 21:46:33 -0400

    Protected keys

--- App.tsx
+++ App.tsx
@@ -12,22 +12,10 @@ import MainTabScreen from './screens/MainTabScreen';
 import RootStackScreen from './screens/RootStackScreen';
 import RunMapScreen from './screens/RunMapScreen';
 import { decode, encode } from 'base-64'
-
+import { firebaseConfig } from './api/Firebase';
 const globalAny: any = global;
 if (!globalAny.btoa) { globalAny.btoa = encode }
 if (!globalAny.atob) { globalAny.atob = decode }
-// require('dotenv').config();
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDL_6h5isQQHhEYW3NX1Z4IB2oq0Yuk_kk",
-  authDomain: "myheart-39fd5.firebaseapp.com",
-  databaseURL: "https://myheart-39fd5.firebaseio.com",
-  projectId: "myheart-39fd5",
-  storageBucket: "myheart-39fd5.appspot.com",
-  messagingSenderId: "714614386613",
-  appId: "1:714614386613:web:1baf39c4d5df13bfe573fc",
-  measurementId: "G-EBPX8R811L"
-};
 
 //firebase.initializeApp(firebaseConfig);
 if (!firebase.apps.length) {
