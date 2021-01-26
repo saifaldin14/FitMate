df6accdf65755471dd7e0735bac3f9fffedb016a
saifaldin14 <saifaldindeveloper@gmail.com>
2021-01-26 09:07:28 -0500

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,35 +1,37 @@
-import React, { memo } from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
+import React, { memo } from "react";
+import { Dimensions, StyleSheet } from "react-native";
 import MapView, { Polyline } from "react-native-maps";
-import { useTheme } from 'react-native-paper';
-import { mapDarkStyle, mapStandardStyle } from '../model/mapData';
+import { useTheme } from "react-native-paper";
+import { mapDarkStyle, mapStandardStyle } from "../model/mapData";
 
-const { width, height } = Dimensions.get('window')
+const { width, height } = Dimensions.get("window");
 
-export const Map = memo(props => {
+export const Map = memo((props) => {
   const theme = useTheme();
 
-  var backGroundColor = '#404040';
+  var backGroundColor = "#404040";
 
   return (
     <MapView
       provider="google"
       style={styles.map}
-      mapType='standard'
+      mapType="standard"
       customMapStyle={theme.dark ? mapDarkStyle : mapStandardStyle}
       showsUserLocation={true}
       followUserLocation={true}
       region={props.getRegion}
-      tintColor='#404040'
-      overlays={[{
-        coordinates: props.routeCoordinates,
-        strokeColor: '#F02A4B',
-        lineWidth: 10,
-      }]}
+      tintColor="#404040"
+      overlays={[
+        {
+          coordinates: props.routeCoordinates,
+          strokeColor: "#F02A4B",
+          lineWidth: 10,
+        },
+      ]}
     >
       <Polyline
         coordinates={props.routeCoordinates}
-        strokeColor='#F02A4B'
+        strokeColor="#F02A4B"
         strokeWidth={8}
       />
     </MapView>
@@ -40,21 +42,6 @@ const styles = StyleSheet.create({
   map: {
     //flex: 0.7,
     width: width,
-    height: height
+    height: height,
   },
 });
-
-/*
-<MapView
-  style={styles.map}
-  mapType='standard'
-  showsUserLocation={true}
-  followUserLocation={true}
-  region={getMapRegion()}
->
-  <Polyline
-    coordinates={state.routeCoordinates}
-    strokeColor='#19B5FE'
-    strokeWidth={8}
-  />
-</MapView>*/
