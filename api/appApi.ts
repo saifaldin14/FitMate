0e462d9a06fe6c69bf542a33fbac01aae2928e1e
saifaldin14 <saifaldindeveloper@gmail.com>
2021-01-26 09:08:48 -0500

    Update appApi.ts

--- api/appApi.ts
+++ api/appApi.ts
@@ -1,43 +1,15 @@
-import '@firebase/firestore';
-import * as firebase from 'firebase';
-//import 'react-native-get-random-values';
-//import { v4 as uuidv4 } from 'uuid';
-//import { uuid as uuidv4 } from 'uuidv4';
-
-/*export function login({ email, password }) {
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((value) => console.log(value))
-}
-
-export function signup({ email, password, displayName }) {
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((userInfo) => {
-      console.log(userInfo)
-      userInfo.user.updateProfile({ displayName: displayName.trim() })
-        .then(() => { })
-    })
-}
-
-export function subscribeToAuthChanges(authStateChanged) {
-  firebase.auth().onAuthStateChanged((user) => {
-    authStateChanged(user);
-  })
-}
-
-export function signout(onSignedOut) {
-  firebase.auth().signOut()
-    .then(() => {
-      onSignedOut();
-    })
-}*/
+import "@firebase/firestore";
+import * as firebase from "firebase";
 
 export function updateData(data, updateComplete) {
   data.updatedAt = firebase.firestore.FieldValue.serverTimestamp();
   console.log("Updating data in firebase");
 
-  firebase.firestore()
-    .collection('Data')
-    .doc(data.id).set(data)
+  firebase
+    .firestore()
+    .collection("Data")
+    .doc(data.id)
+    .set(data)
     .then(() => updateComplete(data))
     .catch((error) => console.log(error));
 }
@@ -45,21 +17,23 @@ export function updateData(data, updateComplete) {
 export function deleteData(data, deleteComplete) {
   console.log(data);
 
-  firebase.firestore()
-    .collection('Data')
-    .doc(data.id).delete()
+  firebase
+    .firestore()
+    .collection("Data")
+    .doc(data.id)
+    .delete()
     .then(() => deleteComplete())
     .catch((error) => console.log(error));
 }
 
 export async function getData(dataRetreived) {
-
   var dataList = [];
 
-  var snapshot = await firebase.firestore()
-    .collection('Data')
-    .orderBy('createdAt')
-    .get()
+  var snapshot = await firebase
+    .firestore()
+    .collection("Data")
+    .orderBy("createdAt")
+    .get();
 
   snapshot.forEach((doc) => {
     const dataItem = doc.data();
@@ -71,16 +45,18 @@ export async function getData(dataRetreived) {
 }
 
 function uuidFunc() {
-  var uuid = "", i, random;
+  var uuid = "",
+    i,
+    random;
 
   for (i = 0; i < 32; i++) {
-    random = Math.random() * 16 | 0;
+    random = (Math.random() * 16) | 0;
 
     if (i == 8 || i == 12 || i == 16 || i == 20) {
       uuid += "-";
     }
 
-    uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
+    uuid += (i == 12 ? 4 : i == 16 ? (random & 3) | 8 : random).toString(16);
   }
 
   return uuid;
@@ -95,7 +71,7 @@ function blobToFile(theBlob, fileName) {
 
 export function uploadData(data, onDataUploaded, { updating }) {
   if (data.imageUri) {
-    const fileExtension = data.imageUri.split('.').pop();
+    const fileExtension = data.imageUri.split(".").pop();
     console.log("EXT: " + fileExtension);
 
     var uuid = uuidFunc();
@@ -104,43 +80,43 @@ export function uploadData(data, onDataUploaded, { updating }) {
     console.log(fileName);
 
     var storageRef = firebase.storage().ref(`data/images/${fileName}`);
-    console.log(typeof (data.imageUri) + " " + data.imageUri);
-    var blob = new Blob([data.imageUri], { type: 'text/plain' });
+    console.log(typeof data.imageUri + " " + data.imageUri);
+    var blob = new Blob([data.imageUri], { type: "text/plain" });
     var file = blobToFile(blob, fileName);
-    storageRef.put(file)
-      .on(
-        firebase.storage.TaskEvent.STATE_CHANGED,
-        snapshot => {
-          console.log("snapshot: " + snapshot.state);
-          console.log("progress: " + (snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+    storageRef.put(file).on(
+      firebase.storage.TaskEvent.STATE_CHANGED,
+      (snapshot) => {
+        console.log("snapshot: " + snapshot.state);
+        console.log(
+          "progress: " + (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
 
-          if (snapshot.state === firebase.storage.TaskState.SUCCESS) {
-            console.log("Success");
-          }
-        },
-        error => {
-          unsubscribe();
-          console.log("image upload error: " + error.toString());
-        },
-        () => {
-          storageRef.getDownloadURL()
-            .then((downloadUrl) => {
-              console.log("File available at: " + downloadUrl);
-
-              data.image = downloadUrl;
-
-              delete data.imageUri;
-
-              if (updating) {
-                console.log("Updating....");
-                updateData(data, onDataUploaded);
-              } else {
-                console.log("adding...");
-                addData(data, onDataUploaded);
-              }
-            })
+        if (snapshot.state === firebase.storage.TaskState.SUCCESS) {
+          console.log("Success");
         }
-      )
+      },
+      (error) => {
+        unsubscribe();
+        console.log("image upload error: " + error.toString());
+      },
+      () => {
+        storageRef.getDownloadURL().then((downloadUrl) => {
+          console.log("File available at: " + downloadUrl);
+
+          data.image = downloadUrl;
+
+          delete data.imageUri;
+
+          if (updating) {
+            console.log("Updating....");
+            updateData(data, onDataUploaded);
+          } else {
+            console.log("adding...");
+            addData(data, onDataUploaded);
+          }
+        });
+      }
+    );
   } else {
     console.log("Skipping image upload");
 
@@ -159,12 +135,14 @@ export function uploadData(data, onDataUploaded, { updating }) {
 export function addData(data, addComplete) {
   data.createdAt = firebase.firestore.FieldValue.serverTimestamp();
 
-  firebase.firestore()
-    .collection('Data')
+  firebase
+    .firestore()
+    .collection("Data")
     .add(data)
     .then((snapshot) => {
       data.id = snapshot.id;
       snapshot.set(data);
-    }).then(() => addComplete(data))
+    })
+    .then(() => addComplete(data))
     .catch((error) => console.log(error));
 }
